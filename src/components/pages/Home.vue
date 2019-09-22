7fe52c035e5f2734dc6c8d5f194ab3f645d24412
Antoine Joly <jolyantoine@zoho.com>
2019-09-22 12:11:08 +0900

    Fetching from API

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="list-section">
         <HomeSection title="Popular">
-          <MangaItem/>
-          <MangaItem/>
-          <MangaItem/>
+          <h4 v-if="loading">Loading...</h4>
+          <MangaItem v-for="Creation in AllCreations"
+              :key="Creation.id"
+              :creation="Creation">
+          </MangaItem>
         </HomeSection>
         <HomeSection title="Recommended">
           <MangaItem/>
@@ -16,12 +18,24 @@
 <script>
 import HomeSection from "../ui/HomeSection.vue";
 import MangaItem from "../ui/MangaItem.vue";
+import { ALL_CREATIONS_QUERY } from '../../graphql/fetch_creations'
 
 export default {
-    components: {
-        HomeSection,
-        MangaItem
+  components: {
+      HomeSection,
+      MangaItem
+  },
+  data () {
+    return {
+      AllCreations: [],
+      loading: 0
     }
+  },
+  apollo: {
+    AllCreations: {
+      query: ALL_CREATIONS_QUERY
+    }
+  }
 }
 </script>
 <style>
