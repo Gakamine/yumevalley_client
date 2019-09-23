6b1a79fdf165256b217f8b49e846a8a17ad88b59
Antoine Joly <jolyantoine@zoho.com>
2019-09-23 11:49:00 +0900

    Better Loading

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,22 +1,20 @@
 <template>
     <div class="list-section">
         <HomeSection title="Popular">
-          <div v-if="loading" class="loading">
-              <MangaItemLoading v-for="load in 4"
-                :key="load"
-              />
-          </div>
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
           <MangaItem v-for="Creation in creation"
               :key="Creation.id"
               :creation_data="Creation">
           </MangaItem>
         </HomeSection>
         <HomeSection title="Recommended">
-          <div class="loading" v-if="loading">
-            <MangaItemLoading v-for="load in 4"
-              :key="load"
-            />
-          </div>
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
+          <MangaItemLoading v-if="loading"  />
           <MangaItem v-for="Creation in creation"
               :key="Creation.id"
               :creation_data="Creation">
@@ -57,8 +55,5 @@ export default {
     width: 100%;
     overflow: hidden;
   }
-}
-.loading {
-  display: flex;
 }
 </style>
