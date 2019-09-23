4fa4fdafc5176f29bce0c3d9099008b04e9a3cf2
Antoine Joly <jolyantoine@zoho.com>
2019-09-23 11:42:20 +0900

    Integration apollo (#15)
    * Integration master on working branch (#9)
    
    * Router (#5)
    
    * Router initial base
    
    * defining basic routes for netlify
    
    * repair vuerouter
    
    * Correcting CSS with routes
    
    * Responsive header (#6)
    
    * Router initial base
    
    * defining basic routes for netlify
    
    * repair vuerouter
    
    * Correcting CSS with routes
    
    * Added media route
    
    * Navbar responsive
    
    * Responsive sidemenu (#7)
    
    * Router initial base
    
    * defining basic routes for netlify
    
    * repair vuerouter
    
    * Correcting CSS with routes
    
    * Added media route
    
    * Navbar responsive
    
    * sidebar responsive
    
    * Responsive css (#8)
    
    * Router initial base
    
    * defining basic routes for netlify
    
    * repair vuerouter
    
    * Correcting CSS with routes
    
    * Added media route
    
    * Navbar responsive
    
    * sidebar responsive
    
    * responsive content
    
    * Update HomeSection.vue
    
    * Fetching from API
    
    * Working fetch creations
    
    * BetterUI
    
    * Better responsive UI
    
    * Repair betterUI
    
    * Fix searchbar
    
    * Loading

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="list-section">
         <HomeSection title="Popular">
+          <div v-if="loading" class="loading">
+              <MangaItemLoading v-for="load in 4"
+                :key="load"
+              />
+          </div>
           <MangaItem v-for="Creation in creation"
               :key="Creation.id"
               :creation_data="Creation">
           </MangaItem>
         </HomeSection>
         <HomeSection title="Recommended">
+          <div class="loading" v-if="loading">
+            <MangaItemLoading v-for="load in 4"
+              :key="load"
+            />
+          </div>
           <MangaItem v-for="Creation in creation"
               :key="Creation.id"
               :creation_data="Creation">
@@ -18,12 +28,14 @@
 <script>
 import HomeSection from "../ui/HomeSection.vue";
 import MangaItem from "../ui/MangaItem.vue";
+import MangaItemLoading from "../ui/MangaItemLoading.vue";
 import { ALL_CREATIONS_QUERY } from '../../graphql/fetch_creations'
 
 export default {
   components: {
       HomeSection,
-      MangaItem
+      MangaItem,
+      MangaItemLoading
   },
   data () {
     return {
@@ -46,10 +58,7 @@ export default {
     overflow: hidden;
   }
 }
-@media screen and (max-width: 1024px) {
-  .list-section {
-    width: 100%;
-    overflow: hidden;
-  }
+.loading {
+  display: flex;
 }
 </style>
---
+++ src/components/ui/MangaItemLoading.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="MangaItem">
+        <div class="MangaItem__cover"/>
+      <div class="MangaItem__name">Loading...</div>
+      <div class="MangaItem__details">
+        <div class="MangaItem__author">Loading...</div>
+        <div class="MangaItem__views">
+          Loading...
+          <EyeIcon/>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { EyeIcon } from "vue-feather-icons";
+
+export default {
+  components: { EyeIcon },
+};
+</script>
+
+<style scoped>
+@media screen and (min-width: 1024px) {
+  .MangaItem__cover {
+    height: 265px;
+    width: 185px;
+  }
+  .MangaItem__details {
+    font-size: 14px;
+  }
+  .MangaItem {
+    margin-right: 42px;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .MangaItem__cover {
+    height: 216px;
+    width: 140px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  .MangaItem__details {
+    font-size: 10px;
+  }
+  .MangaItem {
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+}
+.MangaItem {
+  margin-bottom: 20px;
+}
+.MangaItem__cover {
+  background-image: linear-gradient(-128deg, #b9e4fb 0%, #ffe7f9 100%);
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.03);
+  border-radius: 8px;
+  cursor: pointer;
+  transition-duration: 0.15s;
+  overflow: hidden;
+}
+.MangaItem__cover:hover {
+  transform: scale(1.01) translateY(-1px);
+  box-shadow: 0 8px 14px 0 rgba(0, 0, 0, 0.06);
+}
+.MangaItem__cover:active {
+  transform: none;
+  box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.03);
+}
+.MangaItem__cover img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+.MangaItem__name {
+  margin-top: 15px;
+  color: #444;
+}
+.MangaItem__details {
+  margin-top: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #666666;
+}
+.MangaItem__views {
+  display: flex;
+  align-items: center;
+}
+.MangaItem__views svg {
+  margin-left: 6px;
+  height: 16px;
+  width: 16px;
+}
+.MangaItem__author {
+  text-decoration: none;
+  color: #444;
+}
+</style>
