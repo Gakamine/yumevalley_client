cf5828220da5934ba0c88d803d76fded83b3fc7c
Antoine Joly <jolyantoine@zoho.com>
2020-01-22 19:16:36 +0100

    commit on Master

---
+++ src/components/pages/home.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="list-section">
+        <HomeSection title="Popular">
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItem v-for="Creation in creation"
+              :key="Creation.id"
+              :creation_data="Creation">
+          </CreationItem>
+        </HomeSection>
+        <HomeSection title="Recommended">
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItemLoading v-if="$apollo.queries.creation.loading"  />
+          <CreationItem v-for="Creation in creation"
+              :key="Creation.id"
+              :creation_data="Creation">
+          </CreationItem>
+        </HomeSection>
+      </div>
+</template>
+
+<script>
+import HomeSection from "../ui/HomeSection.vue";
+import CreationItem from "../ui/CreationItem.vue";
+import CreationItemLoading from "../ui/lazy-components/CreationItemLoading.vue";
+import { ALL_CREATIONS_QUERY } from '../../graphql/fetch_creations'
+
+export default {
+  components: {
+      HomeSection,
+      CreationItem,
+      CreationItemLoading
+  },
+  data () {
+    return {
+      creation: []
+    }
+  },
+  apollo: {
+    creation: {
+      query: ALL_CREATIONS_QUERY,
+      error() {
+        this.$parent.APIerror=true
+      }
+    }
+  }
+}
+
+</script>
+<style>
+@media screen and (max-width: 1024px) {
+  .list-section {
+    width: 100%;
+    overflow: hidden;
+  }
+}
+</style>
